b7c120e4cf34dbd6918521a7817bf3512d69b4d9
tomaszmol <tmollegro@gmail.com>
2025-03-17 17:21:09 +0100

    lab1 finished

--- src/power.erl
+++ src/power.erl
@@ -5,3 +5,5 @@
 power(Podstawa, Wykladnik) when Wykladnik == 0 -> 1;
 power(Podstawa, Wykladnik) when Wykladnik > 0 ->  Podstawa * power(Podstawa, Wykladnik - 1);
 power(Podstawa, Wykladnik) when Wykladnik < 0 -> 1/power(Podstawa, -Wykladnik).
+
+% test
